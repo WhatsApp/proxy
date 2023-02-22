062abbf66a63ab63bbbd9e9bf58d25ef07737787
Aaron Sarkissian <aaronsarkissian@gmail.com>
2023-02-23 02:47:35 +0400

    move --max-time to global fetch function

--- proxy/src/set_public_ip_and_start.sh
+++ proxy/src/set_public_ip_and_start.sh
@@ -19,8 +19,9 @@ CONFIG_FILE="/usr/local/etc/haproxy/haproxy.cfg"
 # --show-error: to show errors in the response
 # --fail: to fail on non-200 responses
 # --ipv4: to force ipv4 resolution
+# --max-time: to set a timeout
 function fetch() {
-  curl --silent --show-error --fail --ipv4 "$@"
+  curl --silent --show-error --fail --ipv4 --max-time 2 "$@"
 }
 
 ## PUBLIC_IP supplied from environment variable
@@ -33,7 +34,7 @@ fi
 if [[ $PUBLIC_IP == '' ]]
 then
     # Attempt retrieval of the public ip from the meta-data instance
-    PUBLIC_IP=$(fetch --max-time 2 http://169.254.169.254/latest/meta-data/public-ipv4)
+    PUBLIC_IP=$(fetch http://169.254.169.254/latest/meta-data/public-ipv4)
     if [[ $PUBLIC_IP == '' ]]
     then
         echo "[PROXYHOST] Failed to retrieve public ip address from AWS URI within 2s"
@@ -51,7 +52,7 @@ then
 
     # Attempt retrieval of the public ip from the third-party sources
     for url in "${urls[@]}"; do
-        PUBLIC_IP="$(fetch --max-time 2 "${url}")" && break
+        PUBLIC_IP="$(fetch "${url}")" && break
     done
     if [[ $PUBLIC_IP == '' ]]
     then
